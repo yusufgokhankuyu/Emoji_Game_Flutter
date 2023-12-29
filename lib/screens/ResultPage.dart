1b1c23d9ae6144fa4d2623783ed05c3e5297dfab
Yusuf Gökhan Kuyu <yusufgokhankuyu@gmail.com>
2023-12-29 22:13:39 +0300

    Result Page is created

---
+++ lib/screens/ResultPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key});
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sonuçlar"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
