2debab970acb2c3f7dc5ea0ec81e563b02df7c1f
Yusuf Gökhan Kuyu <yusufgokhankuyu@gmail.com>
2024-01-02 23:34:37 +0300

    Ranking page is added

---
+++ lib/screens/RankingScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RankingScreen extends StatelessWidget {
+  const RankingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ranking Page"),
+      ),
+      body: Container(),
+    );
+  }
+}
